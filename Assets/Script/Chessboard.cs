b530357fa165a7cc8e5a4e74d991f2c857b42b81
JuneMinazuki <xtfrancis@gmail.com>
2025-08-15 11:17:31 +0800

    [Add] Added mesh to each tiles

--- Assets/Script/Chessboard.cs
+++ Assets/Script/Chessboard.cs
@@ -25,6 +25,23 @@ public class Chessboard : MonoBehaviour
         GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
         tileObject.transform.parent = transform;
 
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>();
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, y * tileSize, 0);
+        vertices[1] = new Vector3(x * tileSize, (y+1) * tileSize, 0);
+        vertices[2] = new Vector3((x+1) * tileSize, y * tileSize, 0);
+        vertices[3] = new Vector3((x+1) * tileSize, (y+1) * tileSize, 0);
+
+        int[] tris = new int[] {0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        tileObject.AddComponent<BoxCollider>();
+
         return tileObject;
     }
 }
