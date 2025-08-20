b8b14e80b25599daac70e4114dc137798b9ffc0c
JuneMinazuki <xtfrancis@gmail.com>
2025-08-20 11:33:36 +0800

    [Add] Knight movement

--- Assets/Script/ChessPieces/Knight.cs
+++ Assets/Script/ChessPieces/Knight.cs
@@ -1,6 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : ChessPiece
 {
+    public override List<Vector2Int> GetAvailableMoves(ref ChessPiece[,] board, int tileCountX, int tileCountY)
+    {
+        List<Vector2Int> r = new();
 
+        Vector2Int[] possibleMoves = new Vector2Int[]{new(2, 1), new(2, -1), new (-2, 1), new (-2, -1), new (1, 2), new (-1, 2), new (1, -2), new (-1, -2)};
+
+        foreach (Vector2Int move in possibleMoves)
+        {
+            int nextX = currentX + move.x;
+            int nextY = currentY + move.y;
+
+            // Check if the new position is within the board boundaries.
+            if (nextX >= 1 && nextX < tileCountX - 1 && nextY >= 1 && nextY < tileCountY - 1)
+                // Add the move if the tile is empty or contains an opponent's piece.
+                if (board[nextX, nextY] == null || board[nextX, nextY].team != team)
+                    r.Add(new Vector2Int(nextX, nextY));
+        }    
+
+        return r;
+    }
 }
