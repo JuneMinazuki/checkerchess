996d336faef7ca85868a8c48ea2ed6b4b327382f
JuneMinazuki <xtfrancis@gmail.com>
2025-11-24 09:50:19 +0800

    [Add] Highlighter movement when selecting for side

---
+++ Assets/Script/Menu/SlidingSelector.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SlidingSelector : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public RectTransform highlighter;
+    public RectTransform[] options;
+
+    [Header("Settings")]
+    public float slideSpeed = 10f;
+
+    public int selectedIndex = 0;
+    private RectTransform currentTargetButton;
+
+    void Start()
+    {
+        // Initialize to the first option and set position
+        if(options.Length > 0)
+        {
+            //Force Unity to calculate the layouts
+            Canvas.ForceUpdateCanvases();
+
+            SetOption(0);
+            if (currentTargetButton != null)
+                highlighter.position = currentTargetButton.position;
+        }
+    }
+
+    void Update()
+    {
+        if (currentTargetButton == null) return;
+
+        // Smoothly move the highlighter to the target position
+        highlighter.position = Vector3.Lerp(highlighter.position, currentTargetButton.position, Time.deltaTime * slideSpeed);
+    }
+
+    public void OnButtonClick(int index)
+    {
+        SetOption(index);
+    }
+
+    private void SetOption(int index)
+    {
+        selectedIndex = index;
+        currentTargetButton = options[index];
+    }
+}
